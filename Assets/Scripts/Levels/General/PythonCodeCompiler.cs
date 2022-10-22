a689ed22411042fe2b595886d4922df60f1b0d60
Dexter Downey <crazylegsdexter@gmail.com>
2022-10-22 13:08:50 -0500

    Created working Python compiler

--- Assets/Scripts/Levels/General/PythonCodeCompiler.cs
+++ Assets/Scripts/Levels/General/PythonCodeCompiler.cs
@@ -7,10 +7,14 @@
  * 2. Act upon the results from the compilation.
  * 
  * Author: Robot and I Team
- * Last modification date: 10-20-2022
+ * Credits: All credit for the Python Engine goes to the developers
+ * of the IronPython3 GitHub repository. Thanks to everyone who has
+ * pitched in to help this project succeed.
+ * Last modification date: 10-24-2022
  */
 
 using UnityEngine;
+using IronPython.Hosting;
 using TMPro;
 
 public class PythonCodeCompiler : MonoBehaviour
@@ -25,6 +29,34 @@ public class PythonCodeCompiler : MonoBehaviour
      */
     public void MainDriver()
     {
+        // Local variables
+        Microsoft.Scripting.Hosting.ScriptEngine scriptEngine;
+        Microsoft.Scripting.Hosting.ScriptScope scriptScope;
+        dynamic scriptFunction;
+
+        // Add the player's code to a defined python function for runtime running
+        string playerCode = @"
+# Since Python delimits by whitespace, this is where code must start
+# for whitespace to be properly delimitted.
+
+def main():
+    print('Where does this go?')
+    print('If I can find this, I can pick it up?')
+    return 'Hello World!'
+";
+
+        // Create the engine and define a scope
+        scriptEngine = Python.CreateEngine();
+        scriptScope = scriptEngine.CreateScope();
+
+        // Execute the player's code using the engine and scope
+        scriptEngine.Execute(playerCode, scriptScope);
+
+        // If no compile errors, get the output from the function
+        scriptFunction = scriptScope.GetVariable("main");
+        Debug.Log(scriptFunction());
+
+        // For temporary purposes
         programOutput.text = "Currently unemplemented.\n\n";
         programOutput.text += playerInput.text;
     }
