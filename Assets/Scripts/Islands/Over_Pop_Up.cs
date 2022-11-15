247b513f1540d3fa30de5654cee4bb7c5eb838b0
SporkClay <drake.bouska6842@gmail.com>
2022-11-15 14:35:48 -0600

    Edited Island Overlays, and added pop up menu to PseudoIsland
    Similar Changes to the overworld overlay to add pausing like in levels. Added the popup menu to PseudoIsland.

--- Assets/Scripts/Islands/Over_Pop_Up.cs
+++ Assets/Scripts/Islands/Over_Pop_Up.cs
@@ -12,6 +12,21 @@ namespace GameMechanics
 {
     public class Over_Pop_Up : MonoBehaviour
     {
+        public String ThisLocationLevel; // fill with current scene name
+
+        // link with MenuButton in OverworldOverlay
+        public void PauseWorld()
+        {
+            Time.timeScale = 0;
+        }
+
+        // link with BackButton and ExitWorldButton in MainMenu in LevelMenuOverlay
+        public void UnpauseWorld()
+        {
+            Time.timeScale = 1;
+        }
+
+        // link with ExitLevelButton in MainMenu in LevelMenuOverlay
         public void QuitToTitle()
         {
             StartCoroutine(Load_Level.SceneLoader("MainMenu"));
